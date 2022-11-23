345dc9164ecaa9e589fe3cfa13c7f949890e13b6
victorenrique-gilaltamirano <115146273+victorenrique-gilaltamirano@users.noreply.github.com>
2022-11-23 16:16:14 +0700

    Update image_pub_sub.py

--- OpenCV/image_pub_sub.py
+++ OpenCV/image_pub_sub.py
@@ -12,12 +12,12 @@ bridge = CvBridge()
 def image_callback(ros_image):
   print 'got an image'
   global bridge
-  #convert ros_image into an opencv-compatible image
-  try:
-    cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
-  except CvBridgeError as e:
-      print(e)
-  #from now on, you can work exactly like with opencv
+  #convert ros_image into an opencv-compatible image      ##
+  try:                                                    ##
+    cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")    ##
+  except CvBridgeError as e:                              ##
+      print(e)                                            ##
+  #from now on, you can work exactly like with opencv     ##
   (rows,cols,channels) = cv_image.shape
   if cols > 200 and rows > 200 :
       cv2.circle(cv_image, (100,100),90, 255)

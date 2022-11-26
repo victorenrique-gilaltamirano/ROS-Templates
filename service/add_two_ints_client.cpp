4e0e8f4e8a1786d04df86635a04ec99d28ce16b9
victorenrique-gilaltamirano <115146273+victorenrique-gilaltamirano@users.noreply.github.com>
2022-11-26 11:05:48 +0700

    Update add_two_ints_client.cpp

--- service/add_two_ints_client.cpp
+++ service/add_two_ints_client.cpp
@@ -1,3 +1,17 @@
+/*
+##############
+# Node name: "add_two_ints_client"
+# 
+# Service Client in the "/add_two_ints" topic with the service AddTwoInts.srv definition
+# ~~~~~~~~~~~~~
+# rospy_tutorials/AddTwoInts.srv is defined as:
+# int64 a
+# int64 b
+# ---
+# int64 c
+################
+*/
+
 #include <ros/ros.h>
 #include <rospy_tutorials/AddTwoInts.h>
 

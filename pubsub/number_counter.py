907ce9c0fbd9b70ad078a2e79ae4b27b20d027c1
victorenrique-gilaltamirano <115146273+victorenrique-gilaltamirano@users.noreply.github.com>
2022-11-26 09:32:31 +0700

    Update number_counter.py

--- pubsub/number_counter.py
+++ pubsub/number_counter.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+##############
+# Node name: "number_counter"
+# Subscribed to "/number" topic, carrying the message type Int64.msg
+# Publishing to "/number_count" topic the message type Int64.msg
+# 
+# ~~~~~~~~~~~~~
+# std_msgs/Int64.msg is defined as:
+# int64
+################
+
 import rospy
 from std_msgs.msg import Int64
 
